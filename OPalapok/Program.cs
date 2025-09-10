c2e99de3fd89805e1eeba330b1b97b4d6b75ad85
FodorRobert <fodorr@kkszki.hu>
2025-09-10 09:12:58 +0200

    Tanulo has been added

--- OPalapok/Program.cs
+++ OPalapok/Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace OPalapok
 {
+
+    public class Szemely
+    {
+
+        public string nev = "Peti";
+        public int kor = 34;
+
+        public Szemely(string nev, int kor)
+        {
+
+
+
+        }
+
+        public string kiir()
+        {
+
+            return $"A tanulo neve: {nev} és {kor} éves";
+
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+
+            Szemely tanulo1 = new Szemely("Jani", 55);
+            Console.WriteLine(tanulo1.kiir());
+            Szemely tanulo2 = new Szemely("Ildi", 45);
+            Console.WriteLine(tanulo2.kiir());
         }
     }
 }
